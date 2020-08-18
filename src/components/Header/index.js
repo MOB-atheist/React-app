06e0327731457d65cb439e52eaa9d9abcfbc727c
pablo <pabskne@gmail.com>
2020-08-18 15:31:26 -0300

    Rocket Seat App

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">
+        <div>Rocket Seat App</div>
+    </header>
+)
+
+export default Header
